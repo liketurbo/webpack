56f44ba831a572febf74226e81c0c46b087ddf25
Ramzan Chasygov <theliketurbo@gmail.com>
2019-03-29 23:55:43 +0300

    added nodemon

--- webpack.config.js
+++ webpack.config.js
@@ -10,10 +10,24 @@ const PATHS = {
 };
 
 const commonConfig = {
-  // Entries have to resolve to files! They rely on Node
-  // convention by default so if a directory contains *index.js*,
-  // it resolves to that.
-  bail: true, // stop on the first error,
+  /**
+   * Fixed bug: cannot resolve fs module
+   * https://github.com/webpack-contrib/css-loader/issues/447
+   */
+  node: {
+    fs: 'empty',
+    module: 'empty',
+    child_process: 'empty'
+  },
+  /**
+   * Stop on the first error
+   */
+  bail: true,
+  /**
+   * Entries have to resolve to files! They rely on Node
+   * convention by default so if a directory contains *index.js*,
+   * it resolves to that.
+   */
   entry: {
     app: PATHS.app
   },
@@ -38,18 +52,24 @@ const productionConfig = commonConfig;
 
 const developmentConfig = {
   devServer: {
-    // Enable history API fallback so HTML5 History API based
-    // routing works. Good for complex setups.
+    /**
+     * Enable history API fallback so HTML5 History API based
+     * routing works. Good for complex setups.
+     */
     historyApiFallback: true,
-    // Display only errors to reduce the amount of output.
+    /**
+     * Display only errors to reduce the amount of output.
+     */
     stats: 'errors-only',
-    // Parse host and port from env to allow customization.
-    //
-    // If you use Docker, Vagrant or Cloud9, set
-    // host: options.host || '0.0.0.0';
-    //
-    // 0.0.0.0 is available to all network devices
-    // unlike default `localhost`.
+    /**
+     * Parse host and port from env to allow customization.
+     *
+     * If you use Docker, Vagrant or Cloud9, set
+     * host: options.host || '0.0.0.0';
+     *
+     * 0.0.0.0 is available to all network devices
+     * unlike default `localhost`.
+     */
     host: process.env.HOST, // Defaults to `localhost`
     port: process.env.PORT // Defaults to 8080
   }
