06b6e71a65cb28a09d51db308c769515b0a8fe34
Ramzan Chasygov <theliketurbo@gmail.com>
2019-03-29 22:50:31 +0300

    splited in environments

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const merge = require('lodash/merge');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const FaviconsWebpackPlugin = require('favicons-webpack-plugin');
 const ScriptExtHtmlWebpackPlugin = require('script-ext-html-webpack-plugin');
@@ -8,7 +9,7 @@ const PATHS = {
   build: path.join(__dirname, 'dist')
 };
 
-module.exports = {
+const commonConfig = {
   // Entries have to resolve to files! They rely on Node
   // convention by default so if a directory contains *index.js*,
   // it resolves to that.
@@ -31,4 +32,32 @@ module.exports = {
       path.join(__dirname, 'src/assets/img/icon-4017417_640.png')
     )
   ]
+};
+
+const productionConfig = commonConfig;
+
+const developmentConfig = {
+  devServer: {
+    // Enable history API fallback so HTML5 History API based
+    // routing works. Good for complex setups.
+    historyApiFallback: true,
+    // Display only errors to reduce the amount of output.
+    stats: 'errors-only',
+    // Parse host and port from env to allow customization.
+    //
+    // If you use Docker, Vagrant or Cloud9, set
+    // host: options.host || '0.0.0.0';
+    //
+    // 0.0.0.0 is available to all network devices
+    // unlike default `localhost`.
+    host: process.env.HOST, // Defaults to `localhost`
+    port: process.env.PORT // Defaults to 8080
+  }
+};
+
+module.exports = (_, { mode }) => {
+  if (mode === 'development') {
+    return merge(commonConfig, developmentConfig);
+  }
+  return merge(commonConfig, productionConfig);
 };
