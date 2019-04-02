2d3deeef546ebb46a2b24e95776a6bfdfb488c24
Ramzan Chasygov <theliketurbo@gmail.com>
2019-04-02 23:34:40 +0300

    added styl

--- src/index.js
+++ src/index.js
@@ -2,5 +2,6 @@ import createDiv from './components/create-div';
 import './assets/styles/index.css';
 import './assets/styles/index.less';
 import './assets/styles/index.sass';
+import './assets/styles/index.styl';
 
 document.body.appendChild(createDiv('div'));
--- webpack.config.d/webpack.parts.js
+++ webpack.config.d/webpack.parts.js
@@ -18,6 +18,10 @@ exports.styles = {
       {
         test: /\.sass$/,
         use: ['style-loader', 'css-loader', 'sass-loader']
+      },
+      {
+        test: /\.styl$/,
+        use: ['style-loader', 'css-loader', 'stylus-loader']
       }
     ]
   }
