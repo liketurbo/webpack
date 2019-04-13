c7749807a13709091d2a18f090e4c9b8dafa40b2
Ramzan Chasygov <theliketurbo@gmail.com>
2019-04-13 10:27:39 +0300

    implemented lazy loading

--- src/components/create-div.js
+++ src/components/create-div.js
@@ -3,6 +3,16 @@ import greeting from './greeting';
 const createDiv = text => {
   const element = document.createElement('div');
   element.innerHTML = text || greeting();
+  element.onclick = () => {
+    import('./lazy')
+      .then(lazy => {
+        element.textContent = lazy.default;
+        element.classList.add('lazy');
+      })
+      .catch(err => {
+        console.error(err);
+      });
+  };
   return element;
 };
 
---
+++ src/components/lazy.js
@@ -0,0 +1,2 @@
+import '../assets/styles/lazy.css';
+export default '🖖Hello from lazy';
