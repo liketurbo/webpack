ec867e4ca1065ebd0295cf4c505922cb9f28bd87
Ramzan Chasygov <theliketurbo@gmail.com>
2019-04-22 20:22:38 +0300

    added lodash with implemented tree shaking

--- src/index.js
+++ src/index.js
@@ -10,10 +10,13 @@ import createDiv from './components/create-div';
 import createImage from './components/create-image';
 import kitty from './assets/images/kitty.jpg';
 import murray from './assets/images/murray.jpg';
+import { phrase2 } from './components/texts';
 import rose from './assets/images/rose.jpg';
 import sausage from './assets/images/sausage.jpg';
+import { toUpper } from 'lodash';
 
 document.body.appendChild(createDiv());
+document.body.appendChild(createDiv(toUpper(phrase2())));
 document.body.appendChild(createImage(rose.src, rose.srcSet));
 document.body.appendChild(createImage(kitty.src, kitty.srcSet));
 document.body.appendChild(createImage(murray.src, murray.srcSet));
